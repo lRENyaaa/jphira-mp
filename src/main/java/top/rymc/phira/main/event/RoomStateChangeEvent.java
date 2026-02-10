d214871619d4b3cbdae29ed4a1f9093f7c4b1132
RENaa_FD <92320175+lRENyaaa@users.noreply.github.com>
2026-02-10 19:36:04 +0800

    [CI Build] Fix missing getter on room state change event

--- src/main/java/top/rymc/phira/main/event/RoomStateChangeEvent.java
+++ src/main/java/top/rymc/phira/main/event/RoomStateChangeEvent.java
@@ -1,11 +1,13 @@
 package top.rymc.phira.main.event;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import top.rymc.phira.main.game.Room;
 import top.rymc.phira.main.game.state.RoomGameState;
 import top.rymc.phira.plugin.event.Event;
 
 @RequiredArgsConstructor
+@Getter
 public class RoomStateChangeEvent extends Event {
     private final Room room;
     private final RoomGameState oldState;
